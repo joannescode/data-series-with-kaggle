c0f18ebd72639d6392a57bccd2bd82ac5e7c8f27
Lucas Joannes Vieira Ribeiro <joannesribeiro_@outlook.com>
2025-01-17 20:17:48 -0300

    Chore: docstring para funções de análise

--- unlocking_healthcare_trends_data_analysis/func_analises.py
+++ unlocking_healthcare_trends_data_analysis/func_analises.py
@@ -2,6 +2,15 @@ import matplotlib.pyplot as plt
 
 
 def _ocorrencias_por_genero(df, genero):
+    """Dataframe construido com loc para análise de ocorrências por gênero
+
+    Args:
+        df (dataframe): dataframe contendo as informações a serem analisadas
+        genero (str): parâmetro para consulta 'Female' ou 'Male
+
+    Returns:
+        Dataframe contendo as ocorrências por gênero
+    """
     df_ocorrencias_por_genero = df.loc[
         (df["Gender"] == genero)
         & (df["Medical Condition"].notna())
@@ -12,6 +21,15 @@ def _ocorrencias_por_genero(df, genero):
 
 
 def maiores_ocorrencias_por_genero(df, genero):
+    """Dataframe construido utilizando como consulta o dataframe gerado na função privada '_ocorrencias_por_genero'
+
+    Args:
+        df (dataframe): dataframe retornado da função '_ocorrencias_por_genero'
+        genero (str): parâmetro para consulta 'Female' ou 'Male
+
+    Returns:
+        Dataframe contendo as maiores ocorrências por gênero
+    """
     df_ocorrencias_por_genero = _ocorrencias_por_genero(df, genero)
     maiores_ocorrencias = (
         df_ocorrencias_por_genero.groupby("Medical Condition")
@@ -22,6 +40,17 @@ def maiores_ocorrencias_por_genero(df, genero):
 
 
 def segmentacao_por_entrada_em_hospitais(df, pd, admissao, numero_indicadores):
+    """Dataframe contendo a relação de hospitais que mais tiveram admissões
+
+    Args:
+        df (dataframe): dataframe utilizado para análise
+        pd (modulo): módulo pandas importado
+        admissao (str): parâmetro para consulta 'Elective', 'Urgent' e 'Emergency'
+        numero_indicadores (int): número inteiro para quantidade de ocorrências a serem mostradas
+
+    Returns:
+        Dataframe contendo as maiores ocorrências por hospitais
+    """
     tipos_de_entradas = df["Admission Type"].unique().tolist()
     hospitais_segmentados_por_entradas = (
         df[["Admission Type", "Hospital"]]
@@ -46,6 +75,12 @@ def segmentacao_por_entrada_em_hospitais(df, pd, admissao, numero_indicadores):
 
 
 def grafico_entradas_por_hospital(df, admissao, numero_indicadores):
+    """Indicador de pizza trazendo a relação de tipo de admissão por hospitais
+    Args:
+        df (dataframe): dataframe utilizado para análise
+        admissao (str): parâmetro para consulta 'Elective', 'Urgent' e 'Emergency'
+        numero_indicadores (int): número inteiro para quantidade de ocorrências a serem mostradas
+    """
     filtro_admissao = (df["Admission Type"] == admissao) & (df["Hospital"].notna())
     quantidade_de_entradas = (
         df.loc[filtro_admissao, ["Admission Type", "Hospital"]]
